483c836665f5fd52935118dde0d92a5172397c16
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-17 18:34:24 +0300

    commit tree d34c2f74ad1ae141c593e118a14e09d0d60f0f52

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i=0; i<word.length/2; i++){
+const j=word.length-1-i;
+const startChar=word[i];
+const endChar=word[j];
+if (startChar!==endChar) return false;
+  }
+  return true
 }
+
 
 /* 
   Add your pseudocode here
+  loop through half of the string to check its length
+check if the first and the last string are same
+  return true
+else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  The length of the string is calculated using the length property
+  The for loop is used to iterate up to the half of the string. The if condition is used to check if the first and the corresponding last characters are the same. This loop continues till the half of the string.
+  During the iteration, if any character of the string, when compared with its corresponding last string is not equal, the string is not considered a palindrome.
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
